45c4aaeba4d1f380044700487dae5d1de05778c6
Jorge Matos <jisapjisap@gmail.com>
2025-08-06 16:48:37 +0200

    Refactor page component and add user query functionality
    - Simplified the main page component by removing unnecessary elements and displaying a simple message.
    - Introduced a new `getMany` query in the users.ts file to fetch multiple users from the database.
    - Updated the API definition to include the new users query for better integration.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,11 +4,7 @@ import { add } from "@workspace/math/add"
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(1, 2)}</p>
-      </div>
+      <p>apps/web</p>
     </div>
   )
 }
