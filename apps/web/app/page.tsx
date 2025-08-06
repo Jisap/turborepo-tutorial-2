d3e6994c074b468932c6bc41285ee048179824ac
Jorge Matos <jisapjisap@gmail.com>
2025-08-06 17:39:42 +0200

    Add Convex integration and update dependencies in web and widget apps
    - Integrated Convex for data fetching in both web and widget applications.
    - Updated package.json files to include Convex and backend workspace dependencies.
    - Modified tsconfig.json to include paths for backend.
    - Enhanced page components to display user data fetched from the Convex API.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+"use client"
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+
 
 export default function Page() {
+
+  const users = useQuery(api.users.getMany)
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh">
       <p>apps/web</p>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </div>
     </div>
   )
 }
