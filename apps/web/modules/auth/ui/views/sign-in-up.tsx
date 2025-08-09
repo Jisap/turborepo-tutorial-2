d91e54a3196efdf9edf25a7d07132f4b2d4f215c
Jorge Matos <jisapjisap@gmail.com>
2025-08-09 12:35:30 +0200

    Add Clerk themes support and enhance authentication views
    - Added '@clerk/themes' dependency for theme customization.
    - Updated SignInView and SignUpView components to support dark mode using the resolved theme.
    - Implemented hash routing for better navigation in authentication flows.

--- apps/web/modules/auth/ui/views/sign-in-up.tsx
+++ apps/web/modules/auth/ui/views/sign-in-up.tsx
@@ -1,9 +1,20 @@
-import { SignUp } from '@clerk/nextjs'
+"use client"
 
+import { SignUp } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { useTheme } from 'next-themes'
+
+// Clerk usa las carpetas [[...]] internamente para ofrecer sus páginas por defecto,
+// pero usando routing="hash" permite que el usuario pueda navegar a ellas directamente
 
 export const SignUpView = () => {
+  const { resolvedTheme } = useTheme()
   return (
-    <SignUp />
+    <SignUp 
+      routing="hash" 
+      appearance={{
+        baseTheme: resolvedTheme === 'dark' ? dark : undefined
+      }} 
+    />
   )
 }
-
--- apps/web/modules/auth/ui/views/sign-in-view.tsx
+++ apps/web/modules/auth/ui/views/sign-in-view.tsx
@@ -1,9 +1,21 @@
+"use client"
+
 import { SignIn } from '@clerk/nextjs'
-import React from 'react'
+import { dark } from '@clerk/themes'
+import { useTheme } from 'next-themes'
+
+// Clerk usa las carpetas [[...]] internamente para ofrecer sus páginas por defecto,
+// pero usando routing="hash" permite que el usuario pueda navegar a ellas directamente
 
 export const SignInView = () => {
+  const { resolvedTheme } = useTheme()
+
   return (
-    <SignIn />
+    <SignIn 
+      routing="hash" 
+      appearance={{
+        baseTheme: resolvedTheme === 'dark' ? dark : undefined
+      }}
+    />
   )
 }
-
