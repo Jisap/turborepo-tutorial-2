4ac78b35f3cd47db830ab02f97eb5fe6220bc647
Jorge Matos <jisapjisap@gmail.com>
2025-08-21 17:42:57 +0200

    Enhance WidgetChatScreen with message submission and form handling
    - Integrated form handling using `react-hook-form` and `zod` for message validation.
    - Implemented message submission functionality with `createMessage` action, allowing users to send messages in the chat.
    - Updated UI to include a form for user input and display AI responses, improving interaction within the chat interface.
    - Added validation to ensure messages are required before submission, enhancing user experience.

--- apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
@@ -7,7 +7,12 @@ import { ArrowLeftIcon, MenuIcon } from "lucide-react";
 import { useAtomValue, useSetAtom } from "jotai";
 import { contactSessionIdAtomFamily, conversationIdAtom, organizationIdAtom, screenAtom } from "../../atoms/widget-atoms";
 import { api } from "@workspace/backend/_generated/api";;
-import { useQuery } from "convex/react";
+import { useAction, useQuery } from "convex/react";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { Form, FormField } from '@workspace/ui/components/form';
+import { AIResponse } from '../../../../../../packages/ui/src/components/ai/response';
 import { 
   AIConversation,
   AIConversationContent,
@@ -28,6 +33,10 @@ import {
   AISuggestion,
   AISuggestions,
 } from '@workspace/ui/components/ai/suggestion';
+
+const formSchema = z.object({
+  message: z.string().min(1, 'Message is required' )
+});
 
 export const WidgetChatScreen = () => {
 
@@ -63,10 +72,28 @@ export const WidgetChatScreen = () => {
     }
   );
 
-
   const onBack = () => {
     setConversationId(null);
     setScreen("selection");
+  }
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { message: '' },
+  });
+
+  const createMessage = useAction(api.public.messages.create);
+  
+  const onSubmit = async(values: z.infer<typeof formSchema>) => {
+    if (!conversation || !contactSessionId) return;
+
+    form.reset();
+
+    await createMessage({
+      threadId: conversation.threadId,
+      prompt: values.message,
+      contactSessionId,
+    });
   }
 
   return (
@@ -91,12 +118,65 @@ export const WidgetChatScreen = () => {
         </Button>
       </WidgetHeader>
 
-      <div className="flex flex-1 flex-col gap-y-4 p-4">
-        <p className="text-sm">
-          {JSON.stringify(conversation)}
-          {JSON.stringify(messages)}
-        </p>
-      </div>
+      <AIConversation>
+        <AIConversationContent>
+          {toUIMessages(messages.results ?? [])?.map((message) => (
+            <AIMessage
+              from={message.role === 'user' ? 'user' : 'assistant'}
+              key={message.id}
+            >
+              <AIMessageContent>
+                <AIResponse>{message.content}</AIResponse>
+              </AIMessageContent>
+              {/* TODO: Add Avatar component */}
+            </AIMessage>
+          ))}
+        </AIConversationContent>
+      </AIConversation>
+      {/* TODO: Add suggestions */}
+
+      <Form {...form}>
+        <AIInput 
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='rounded-none border-x-0 border-b-0'
+        >
+          <FormField
+            control={form.control}
+            disabled={conversation?.status === 'resolved'}
+            name="message"
+            render={({ field }) => (
+              <AIInputTextarea
+                disabled={conversation?.status === 'resolved'}
+                onChange={field.onChange}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' && !e.shiftKey) {
+                    e.preventDefault();
+
+                    form.handleSubmit(onSubmit)();
+                  }
+                }}
+                placeholder={
+                  conversation?.status === 'resolved'
+                    ? 'This conversation has been resolved'
+                    : 'Type your messages...'
+                }
+                value={field.value}
+              />
+            )}
+          />
+          
+          <AIInputToolbar>
+            <AIInputTools />
+            <AIInputSubmit
+              type="submit"
+              disabled={
+                conversation?.status === 'resolved' || !form.formState.isValid
+              }
+              status="ready"
+            />
+          </AIInputToolbar>
+        </AIInput>
+      </Form>
     </>
   )
 }
