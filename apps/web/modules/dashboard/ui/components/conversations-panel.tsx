6c4573a7f8b8ccacc2a7a2518a711a8ce8915864
Jorge Matos <jisapjisap@gmail.com>
2025-08-24 17:44:51 +0200

    Add countries-and-timezones dependency and implement country utility functions
    - Added `countries-and-timezones` package to manage country and timezone data.
    - Created utility functions `getCountryFromTimezone` and `getCountryFlagUrl` for retrieving country information based on timezones and generating flag URLs.
    - Updated `ConversationsPanel` to display country flags alongside conversation avatars, enhancing user experience.
    - Included a new SVG logo file for branding in the application.

--- apps/web/modules/dashboard/ui/components/conversations-panel.tsx
+++ apps/web/modules/dashboard/ui/components/conversations-panel.tsx
@@ -17,10 +17,18 @@ import {
 import { ScrollArea } from '@workspace/ui/components/scroll-area'
 import { api } from '@workspace/backend/_generated/api';
 import { usePaginatedQuery } from 'convex/react';
-
+import { getCountryFlagUrl, getCountryFromTimezone } from '@/lib/country';
+import Link from 'next/link';
+import { cn } from '@workspace/ui/lib/utils';
+import { usePathname } from 'next/navigation';
+import { DicebearAvatar } from '../../../../../../packages/ui/src/components/dicebear-avatar';
+import { formatDistanceToNow } from 'date-fns'
+import ConversationStatusIcon from '../../../../../../packages/ui/src/components/conversation-status-icon';
 
 
 const ConversationsPanel = () => {
+
+  const pathname = usePathname()
 
   const conversations = usePaginatedQuery( // Usamos la función usePaginatedQuery para obtener una lista paginada de conversaciones
     api.private.conversations.getMany,
@@ -75,7 +83,79 @@ const ConversationsPanel = () => {
 
       <ScrollArea className="max-h-[calc(100vh-53px)]">
         <div className="flex w-full flex-1 flex-col text-sm">
-          {JSON.stringify(conversations)}
+          {conversations.results.map((conversation) => {
+
+            const isLastMessageFromOperator =
+              conversation.lastMessage?.message?.role !== 'user'
+
+            const country = getCountryFromTimezone(
+              conversation.contactSession.metadata?.timezone,
+            )
+
+            const countryFlagUrl = country?.code
+              ? getCountryFlagUrl(country.code)
+              : undefined
+
+            return (
+              <Link
+                key={conversation._id}
+                href={`/conversations/${conversation._id}`}
+                className={cn(
+                  'relative flex cursor-pointer items-start gap-3 border-b p-4 py-5 text-sm leading-tight hover:bg-accent hover:text-accent-foreground',
+                  pathname === `/conversations/${conversation._id}` &&
+                  'bg-accent text-accent-foreground',
+                )}
+              >
+                {/* Indicador de la conversación activa */}
+                <div
+                  className={cn(
+                    '-translate-y-1/2 absolute top-1/2 left-0 h-[64%] w-1 rounded-r-full bg-neutral-300 opacity-0 transition-opacity',
+                    pathname === `/conversations/${conversation._id}` &&
+                    'opacity-100',
+                  )}
+                />
+
+                <DicebearAvatar
+                  seed={conversation.contactSession._id}
+                  size={40}
+                  className="shrink-0"
+                  badgeImageUrl={countryFlagUrl}
+                />
+
+                <div className='flex-1'>
+                  <div className="flex w-full items-center gap-2">
+                    <span className="truncate">
+                      {conversation.contactSession.name}
+                    </span>
+                    <span className="ml-auto shrink-0 text-muted-foreground text-xs">
+                      {formatDistanceToNow(conversation._creationTime)}
+                    </span>
+                  </div>
+
+                  <div className="mt-1 flex items-center justify-between gap-2">
+                    <div className="flex w-0 grow items-center gap-1">
+                      {isLastMessageFromOperator && (
+                        <CornerUpLeftIcon className="size-3 shrink-0 text-muted-foreground" />
+                      )}
+                      <span
+                        className={cn(
+                          'line-clamp-1 text-muted-foreground text-xs',
+                          !isLastMessageFromOperator &&
+                          'font-bold text-black',
+                        )}
+                      >
+                        {conversation.lastMessage?.text}
+                      </span>
+                    </div>
+
+                    <ConversationStatusIcon status={conversation.status} />
+                  </div>
+
+                </div>
+                
+              </Link>
+            )
+          })}
         </div>
       </ScrollArea>
     </div>
