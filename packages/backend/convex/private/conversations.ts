bf44b46680d621b32cf03e8a134aab0923ae6b3b
Jorge Matos <jisapjisap@gmail.com>
2025-08-27 16:59:25 +0200

    Add updateStatus mutation for conversation management
    - Introduced `updateStatus` mutation to allow updating the status of conversations.
    - Implemented user authentication and organization validation to ensure secure access.
    - Added error handling for unauthorized access and conversation existence checks, enhancing reliability and security.

--- packages/backend/convex/private/conversations.ts
+++ packages/backend/convex/private/conversations.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { query } from "../_generated/server";
+import { mutation, query } from "../_generated/server";
 import { supportAgent } from "../system/ai/agents/supportAgent";
 import { MessageDoc, saveMessage } from "@convex-dev/agent";
 
@@ -110,6 +110,54 @@ export const getMany = query({ // No es necesario el contactSessionId porque est
    
   },
 });
+
+export const updateStatus = mutation({
+  args: {
+    conversationId: v.id('conversations'),
+    status: v.union(
+      v.literal('unresolved'),
+      v.literal('escalated'),
+      v.literal('resolved')
+    ),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+    if (identity === null) {
+      throw new ConvexError({
+        code: 'UNAUTHORIZED',
+        message: 'Identity not found',
+      })
+    }
+
+    const orgId = identity.orgId as string
+    if (!orgId) {
+      throw new ConvexError({
+        code: 'UNAUTHORIZED',
+        message: 'Organization not found',
+      })
+    }
+
+    const conversation = await ctx.db.get(args.conversationId)
+    if (!conversation) {
+      throw new ConvexError({
+        code: 'NOT_FOUND',
+        message: 'Conversation not found',
+      })
+    }
+
+    if (conversation.organizationId !== orgId) {
+      throw new ConvexError({
+        code: 'UNAUTHORIZED',
+        message: 'Invalid Organization ID',
+      })
+    }
+
+    await ctx.db.patch(args.conversationId, {
+      status: args.status
+    })
+
+  }
+})
 
 
 export const getOne = query({
